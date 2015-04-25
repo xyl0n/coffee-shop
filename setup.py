80ca05912e687812e377c02337de92f3f8076b3e
xyl0n <avlabs314@gmail.com>
2015-04-25 17:27:32 +0100

    Added a desktop file to launch from application menus etc.

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ data = [ ('/usr/share/acoustics', glob.glob('acoustics/*')),
          ('/usr/bin', glob.glob ('bin/*')),
          ('/usr/share/acoustics/icons', glob.glob('share/acoustics/icons/*')),
          ('/usr/share/acoustics/sounds', glob.glob('share/acoustics/sounds/*')),
+         ('/usr/share/icons/hicolor/scalable/apps', glob.glob('share/icons/scalable/*')),
+         ('/usr/share/applications', glob.glob('share/acoustics.desktop')),
          ('/usr/share/acoustics', glob.glob('share/acoustics/mpv.conf')) ]
 
 # Setup stage
 DistUtilsExtra.auto.setup(
     name         = "acoustics",
-    version      = "0.0.1",
+    version      = "0.1",
     description  = "Acoustics Noise Player",
     author       = "Aporva Varshney",
     author_email = "",
